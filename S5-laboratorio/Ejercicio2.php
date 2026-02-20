10a9ba6af449ef10a8e14fdb27ba14e7514b64a5
porcramines <porfiriones@gmail.com>
2026-02-20 09:59:41 -0500

    SESION5

---
+++ S5-laboratorio/Ejercicio2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<h1>Esto es un <?php # echo 'simple'; ?> ejemplo</h1>
+<p>El encabezado anterior dirá 'Esto es un ejemplo'.</p>
+</body>
+</html>
---
+++ S5-laboratorio/Ejercicio3.php
@@ -0,0 +1,8 @@
+<?php
+$clave = "xyz345"; //recordar avisar al gerente si la cambiamos
+$tolerancia = 3; #es la cantidad de reintentos de ingreso tolerados con una contraseña equivocada
+
+echo $clave; /*recordar que si mostramos esto,
+ previamente deberíamos informar al usuario,
+ para que no lo exponga a la vista de otros. */
+?>
